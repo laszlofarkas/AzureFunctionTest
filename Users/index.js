6e19aaa7fd14a5fd9e3ea8f9a9d1723f789eac30
László Farkas <laszlo.farkas@nebu.com>
2016-11-12 01:08:34 +0100

    Determine request

---
+++ Users/index.js
@@ -0,0 +1,19 @@
+module.exports = function (context, req) {
+
+    context.log('JavaScript HTTP trigger function processed a request.');
+	context.log('request', req);
+
+    /*if (req.query.name || (req.body && req.body.name)) {
+        res = {
+            // status: 200, 
+            body: "Hello " + (req.query.name || req.body.name) + " this is a JS based tool!"
+        };
+    }
+    else {
+        res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }*/
+    context.done(null, res);
+} 
