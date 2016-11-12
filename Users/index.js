a838f77e3bf46517a03140599d251c3eb06aca21
László Farkas <laszlo.farkas@nebu.com>
2016-11-12 02:46:10 +0100

    Users HEAD, PUT, GET (memoryDB)

--- Users/index.js
+++ Users/index.js
@@ -1,9 +1,11 @@
+var users = [];
+
 module.exports = function (context, req) {
 
     context.log('JavaScript HTTP trigger function processed a request.');
 
-	var id = context.bindingData.id;
-	context.log('Method: ', req.method, " id: ", id);
+	var id = parseInt(context.bindingData.id);
+	context.log('Method: ', req.method, "id:", id, typeof(id));
 
 	var res;
 
@@ -14,25 +16,42 @@ module.exports = function (context, req) {
 				body: "Missing route parameter"
 			};
 		} else {
-			if (id === 2) {
-				res = {status: 200};
+			if (users.some(function(e) {return e.id == id;})) {
+				res = {
+					status: 200,
+					body: "Found"
+				};
 			} else {
-				res = {status: 404};
+				res = {
+					status: 404,
+					body: "Not found"
+				};
 			}
 		}
 	}
 
 	if (req.method === 'PUT') {
-		
+		if (req.body && req.body.id && req.body.name) {
+			users.push({id: req.body.id, name: req.body.name});
+			res = {body: req.body.name + " has been added"};
+		} else {
+			res = {
+				status: 400,
+				body: (req.body && typeof(req.body) === "object") 
+					? "id and name are required parameter"
+					: "invalid json"
+			};
+		}
+		context.log(users);
 	}
 
 	if (req.method === 'GET') {
-		
+		res = {body: users};
 	}
 
 	if (req.method === 'DELETE') {
 		
 	}
-
+	
     context.done(null, res);
 } 
