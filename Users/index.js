4b2742c5e82dac21e7ba90343f84ed2f6f254ea4
László Farkas <laszlo.farkas@nebu.com>
2016-11-12 01:26:33 +0100

    User Head metod (dummy)

--- Users/index.js
+++ Users/index.js
@@ -6,9 +6,20 @@ module.exports = function (context, req) {
 	context.log('Method: ', req.method, " id: ", id);
 
 	var res;
-/*
+
 	if (req.method === 'HEAD') {
-		if ()
+		if (id == null) {
+			res = {
+				status: 400,
+				body: "Missing route parameter"
+			};
+		} else {
+			if (id === 2) {
+				res = {status: 200};
+			} else {
+				res = {status: 404};
+			}
+		}
 	}
 
 	if (req.method === 'PUT') {
@@ -22,6 +33,6 @@ module.exports = function (context, req) {
 	if (req.method === 'DELETE') {
 		
 	}
-*/
+
     context.done(null, res);
 } 
