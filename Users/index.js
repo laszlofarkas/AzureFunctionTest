8f902dfeb11100bb6e8afff4102fbe08214e3671
László Farkas <laszlo.farkas@nebu.com>
2016-11-22 22:47:04 +0100

    move nodemoduls to libs folder

--- Users/index.js
+++ Users/index.js
@@ -1,11 +1,42 @@
 var users = [];
 
+var Connection = require('../lib/tedious').Connection;
+var Request = require('../lib/tedious').Request;
+
+
 module.exports = function (context, req) {
 
-    context.log('JavaScript HTTP trigger function processed a request.');
+	context.log('JavaScript HTTP trigger function processed a request.');
+
+	context.log(process.env["SQLAZURECONNSTR_TestDB"]);
+	context.log(JSON.parse(process.env["SQLAZURECONNSTR_TestDB"]));
+
+	var connection = new Connection(JSON.parse(process.env["SQLAZURECONNSTR_TestDB"]));
+	connection.on('connect', function (err) {
+		executeStatement();
+	});
+
+	function executeStatement() {
+		var request = new Request("select 42, 'hello world'", function (err, rowCount) {
+			if (err) {
+				context.log(err);
+			} else {
+				context.log(rowCount + ' rows');
+			}
+		});
+
+		request.on('row', function (columns) {
+			columns.forEach(function (column) {
+				context.log(column.value);
+			});
+		});
+
+		connection.execSql(request);
+	}
+
 
 	var id = parseInt(context.bindingData.id);
-	context.log('Method: ', req.method, "id:", id, typeof(id));
+	context.log('Method: ', req.method, "id:", id, typeof (id));
 
 	var res;
 
@@ -16,7 +47,7 @@ module.exports = function (context, req) {
 				body: "Missing route parameter"
 			};
 		} else {
-			if (users.some(function(e) {return e.id == id;})) {
+			if (users.some(function (e) { return e.id == id; })) {
 				res = {
 					status: 200,
 					body: "Found"
@@ -32,12 +63,12 @@ module.exports = function (context, req) {
 
 	if (req.method === 'PUT') {
 		if (req.body && req.body.id && req.body.name) {
-			users.push({id: req.body.id, name: req.body.name});
-			res = {body: req.body.name + " has been added"};
+			users.push({ id: req.body.id, name: req.body.name });
+			res = { body: req.body.name + " has been added" };
 		} else {
 			res = {
 				status: 400,
-				body: (req.body && typeof(req.body) === "object") 
+				body: (req.body && typeof (req.body) === "object")
 					? "id and name are required parameter"
 					: "invalid json"
 			};
@@ -46,12 +77,12 @@ module.exports = function (context, req) {
 	}
 
 	if (req.method === 'GET') {
-		res = {body: users};
+		res = { body: users };
 	}
 
 	if (req.method === 'DELETE') {
-		
+
 	}
-	
-    context.done(null, res);
-} 
+
+	context.done(null, res);
+}
