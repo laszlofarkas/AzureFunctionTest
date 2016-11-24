3ec5427b492219a6dca344a7d093a2c3ab98fd1c
László Farkas <laszlo.farkas@nebu.com>
2016-11-24 02:14:01 +0100

    CRD user - semi final

--- Users/index.js
+++ Users/index.js
@@ -1,88 +1,117 @@
-var users = [];
-
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES; 
 
+var UserService = {
+	
+	getUserlist: function() {
+		return new Promise(function(resolve, reject) {
+			var result = [];
+			var connection = new Connection(JSON.parse(process.env["SQLAZURECONNSTR_TestDB"]));
+			connection.on('connect', function (err) {
+				var request = new Request("SELECT id, name FROM Users", function (err, rowCount) {
+					if (err) {
+						reject(err);
+					} else {
+						resolve(result);
+					}
+				});
+				request.on('row', function (columns) {
+					result.push(
+						{
+							"id": columns[0].value,
+							"name": columns[1].value
+						}
+					);
+				});
+				connection.execSql(request);
+			});
+		});
+	},
+	
+	addUser: function(newUser) {
+		return new Promise(function(resolve, reject) {
+			var connection = new Connection(JSON.parse(process.env["SQLAZURECONNSTR_TestDB"]));
+			connection.on('connect', function (err) {
+				var request = new Request("INSERT INTO Users (name) VALUES (@name)", function(err) {
+					if (err) {
+						reject(err);
+					} else {
+						resolve();
+					}
+				});
+				request.addParameter('name', TYPES.NVarChar, newUser.name);
+				connection.execSql(request);
+			});
+		});
+	},
+	
+	deleteUser: function(id) {
+		return new Promise(function(resolve, reject) {
+			var connection = new Connection(JSON.parse(process.env["SQLAZURECONNSTR_TestDB"]));
+			connection.on('connect', function (err) {
+				var request = new Request("DELETE Users WHERE id = @id", function(err) {
+					if (err) {
+						reject(err);
+					} else {
+						resolve();
+					}
+				});
+				request.addParameter('id', TYPES.Int, id);
+				connection.execSql(request);
+			});
+		});
+	}
+}
 
 module.exports = function (context, req) {
 
 	context.log('JavaScript HTTP trigger function processed a request.');
 
-	context.log(process.env["SQLAZURECONNSTR_TestDB"]);
-	context.log(JSON.parse(process.env["SQLAZURECONNSTR_TestDB"]));
-
-	var connection = new Connection(JSON.parse(process.env["SQLAZURECONNSTR_TestDB"]));
-	connection.on('connect', function (err) {
-		executeStatement();
-	});
-
-	function executeStatement() {
-		var request = new Request("select 42, 'hello world'", function (err, rowCount) {
-			if (err) {
-				context.log(err);
-			} else {
-				context.log(rowCount + ' rows');
-			}
-		});
-
-		request.on('row', function (columns) {
-			columns.forEach(function (column) {
-				context.log(column.value);
-			});
-		});
-
-		connection.execSql(request);
-	}
-
-
 	var id = parseInt(context.bindingData.id);
 	context.log('Method: ', req.method, "id:", id, typeof (id));
 
-	var res;
-
-	if (req.method === 'HEAD') {
-		if (id == null) {
-			res = {
-				status: 400,
-				body: "Missing route parameter"
-			};
-		} else {
-			if (users.some(function (e) { return e.id == id; })) {
-				res = {
-					status: 200,
-					body: "Found"
-				};
-			} else {
-				res = {
-					status: 404,
-					body: "Not found"
-				};
-			}
-		}
-	}
-
-	if (req.method === 'PUT') {
-		if (req.body && req.body.id && req.body.name) {
-			users.push({ id: req.body.id, name: req.body.name });
-			res = { body: req.body.name + " has been added" };
-		} else {
-			res = {
-				status: 400,
-				body: (req.body && typeof (req.body) === "object")
-					? "id and name are required parameter"
-					: "invalid json"
-			};
-		}
-		context.log(users);
-	}
-
 	if (req.method === 'GET') {
-		res = { body: users };
+		UserService.getUserlist().then(function(users) {
+			context.done(null, { body: users });
+		}).catch(function(err) {
+			context.log(err);
+			context.done(err, null);
+		});
 	}
-
+	
+	if (req.method === 'PUT') {
+		if (req.body && req.body.name) {
+			UserService.addUser(req.body).then(function() {
+				context.done();
+			}).catch(function(err) {
+				context.log(err);
+				context.done(err);
+			});
+		} else {
+			var res = {
+				status: 400,
+				body: "invalid json"
+			};
+			context.done(null, res);
+		}
+	}
+	
 	if (req.method === 'DELETE') {
-
+		var id = parseInt(context.bindingData.id);
+		if (id && typeof id === "number") {
+			UserService.deleteUser(id).then(function() {
+				context.done();
+			}).catch(function(err) {
+				context.log(err);
+				context.done(err);			
+			})
+		} else {
+			var res = {
+				status: 400,
+				body: "missing parameter: id"
+			};
+			context.done(null, res);
+		}
 	}
-
-	context.done(null, res);
 }
